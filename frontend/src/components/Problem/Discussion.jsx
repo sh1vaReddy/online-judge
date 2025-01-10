e850584e49def24dcd3d7c8fc36e45de2b59a5e7
admin <kanagadashivareddy@gmail.com>
2025-01-10 13:52:15 +0530

    dark mode

--- frontend/src/components/Problem/Discussion.jsx
+++ frontend/src/components/Problem/Discussion.jsx
@@ -42,26 +42,28 @@ const Discussion = () => {
 
   return (
     <div className="flex flex-col h-[550px] bg-gray-100">
-      <div className="flex-grow overflow-auto p-4">
-        <h1 className="text-xl font-semibold mb-4">Welcome to the discussion</h1>
-        <ul className="space-y-4">
+      <div className="flex-grow overflow-auto p-4 dark:bg-gray-700">
+        <h1 className="text-xl font-semibold mb-4 ">Welcome to the discussion</h1>
+        <ul className="space-y-4 ">
           {discussions.map((item, index) => (
-            <li key={index} className="bg-white shadow-md rounded-md p-4">
-              <h2 className="text-lg font-bold text-gray-700">{item.user_name}</h2>
-              <p className="text-gray-600">{item.content}</p>
+            <li key={index} className="bg-white shadow-md rounded-md p-4 dark:bg-gray-600">
+              <h2 className="text-lg font-bold text-gray-700 dark:text-white">{item.user_name}</h2>
+              <p className="text-gray-600 dark:text-gray-100">{item.content}</p>
             </li>
           ))}
         </ul>
       </div>
 
-      <div className="p-4 bg-white shadow-md sticky bottom-0">
+      <div className="p-4 bg-white shadow-md sticky bottom-0 dark:bg-gray-700">
         <div className="flex items-center gap-2">
           <input
             type="text"
             value={content}
             onChange={(e) => setContent(e.target.value)}
             onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
-            className="flex-grow border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="flex-grow border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500
+            dark:text-black
+            "
             placeholder="Type your message..."
             aria-label="Type your message"
           />
